161ac916b2bc07a7c550ea38cefe4bc859fb2235
Paweł Wysocki <01178616@pw.edu.pl>
2026-04-28 11:58:53 +0200

    Configure frontend

---
+++ infrastructure/frontend.tf
@@ -0,0 +1,113 @@
+# S3 bucket for frontend files
+resource "aws_s3_bucket" "frontend" {
+  bucket        = "sd-frontend-dev"
+  force_destroy = true
+}
+
+# Block public access to S3 (everything goes through CloudFront)
+resource "aws_s3_bucket_public_access_block" "frontend_nodes" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# CloudFront Origin Access Control (S3 access)
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "sd-frontend-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "frontend_dist" {
+  origin {
+    domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id                = "S3-Frontend"
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  # React Router servicing (redirect 403/404 to index.html)
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+  
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Frontend"
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# Policy to allow CloudFront to read S3 files
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend_dist.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+output "frontend_url" {
+  value = "https://${aws_cloudfront_distribution.frontend_dist.domain_name}"
+}
+
+output "frontend_bucket_name" {
+  description = "Frontend bucket name"
+  value       = aws_s3_bucket.frontend.id
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront Distribution ID"
+  value       = aws_cloudfront_distribution.frontend_dist.id
+}

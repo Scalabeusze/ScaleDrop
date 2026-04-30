c3bc782e120eb1738c8f85ec4636957f2f773941
XeroChan <maciej2013.mp@gmail.com>
2026-04-30 09:23:50 +0200

    Added redirect uri

--- file-manager-app/src/pages/Public/LoginPage.jsx
+++ file-manager-app/src/pages/Public/LoginPage.jsx
@@ -13,7 +13,7 @@ export const LoginPage = () => {
 
   const handleOAuthLogin = () => {
     const clientId = '393425779945-5hhvuddp5l97dokdl6kshg7gtdceo1lq.apps.googleusercontent.com'; 
-    const redirectUri = encodeURIComponent(window.location.origin + '/oauth-callback');
+    const redirectUri = 'http://sd-alb-dev-1442333574.eu-north-1.elb.amazonaws.com/sd-iam/login/oauth2/code/google';
     const scope = encodeURIComponent('openid email profile');
     const responseType = 'code';
     const state = 'standard_oauth_state_string_mock'; 

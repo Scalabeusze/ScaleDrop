0f269907d2a45817342ecf72f4a4a6564eb12667
Paweł Wysocki <Pawel.Wysocki@circlekeurope.com>
2026-05-13 21:04:02 +0200

    Try to fix mixed content

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -24,10 +24,25 @@ resource "aws_cloudfront_origin_access_control" "default" {
 
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "frontend_dist" {
+
+  # S3 origin
   origin {
     domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
     origin_id                = "S3-Frontend"
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+  }
+
+  # ALB origin
+  origin {
+    domain_name = aws_lb.main.dns_name
+    origin_id   = "Backend-ALB"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   enabled             = true
@@ -40,13 +55,28 @@ resource "aws_cloudfront_distribution" "frontend_dist" {
     response_code      = 200
     response_page_path = "/index.html"
   }
-  
+
   custom_error_response {
     error_code         = 404
     response_code      = 200
     response_page_path = "/index.html"
   }
 
+  # Route traffic to ALB
+  ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    target_origin_id = "Backend-ALB"
+
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    origin_request_policy_id = "216adef6-5c7f-47e4-b362-e7f0f4fe336a"
+  }
+
+  # Default behavior - route traffic to S3
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
@@ -81,13 +111,13 @@ resource "aws_s3_bucket_policy" "allow_cloudfront" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AllowCloudFrontServicePrincipalReadOnly"
-        Effect = "Allow"
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
         Principal = {
           Service = "cloudfront.amazonaws.com"
         }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = aws_cloudfront_distribution.frontend_dist.arn

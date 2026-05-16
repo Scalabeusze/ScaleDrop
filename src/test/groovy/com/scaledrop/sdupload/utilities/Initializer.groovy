69b5caf1fd2053acabd10ae1da949db58339114c
Depermitto <penciller@disroot.org>
2026-05-16 17:49:30 +0200

    chore(fmt): add license header, adjust spotless

--- src/test/groovy/com/scaledrop/sdupload/utilities/Initializer.groovy
+++ src/test/groovy/com/scaledrop/sdupload/utilities/Initializer.groovy
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2026-present Scalabeusze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.scaledrop.sdupload.utilities
 
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SNS
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS
+
 import groovy.util.logging.Slf4j
 import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.context.ApplicationContextInitializer
@@ -15,10 +32,10 @@ import org.testcontainers.utility.DockerImageName
 class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
   public static final LocalStackContainer AWS_CONTAINER =  new LocalStackContainer(DockerImageName
-      .parse("localstack/localstack:3.1.0"))
-      .withClasspathResourceMapping("/localstack", "/etc/localstack/init/ready.d", BindMode.READ_ONLY)
-      .withServices(SQS, SNS)
-      .waitingFor(Wait.forLogMessage(".*Ready\\.\n", 1));
+  .parse("localstack/localstack:3.1.0"))
+  .withClasspathResourceMapping("/localstack", "/etc/localstack/init/ready.d", BindMode.READ_ONLY)
+  .withServices(SQS, SNS)
+  .waitingFor(Wait.forLogMessage(".*Ready\\.\n", 1));
 
   static {
     AWS_CONTAINER.start();

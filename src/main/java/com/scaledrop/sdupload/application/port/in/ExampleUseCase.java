bbe2706ebe78dcaadc8b55fbf57a0d2a4368770c
Depermitto <penciller@disroot.org>
2026-05-16 17:47:34 +0200

    chore: rm example code, run pre-commit

--- src/main/java/com/scaledrop/sdupload/application/port/in/ExampleUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.scaledrop.sdupload.application.port.in;
-
-import com.scaledrop.sdupload.domain.example.ExampleObject;
-
-public interface ExampleUseCase {
-
-  ExampleObject getExampleObject();
-}

bd2aef2f5eb93b80b2e482d55a79bbc840c7d82c
Paweł Wysocki <01178616@pw.edu.pl>
2026-05-19 18:08:31 +0200

    Fix type

--- infrastructure/autoscale.tf
+++ infrastructure/autoscale.tf
@@ -1,6 +1,6 @@
 variable "enable_ecs_schedule" {
   description = "Whether to turn on autoscale"
-  type        = boolean
+  type        = bool
   default     = true
 }
 
@@ -15,10 +15,10 @@ locals {
 
 # Register all services in Auto Scaling
 resource "aws_appautoscaling_target" "ecs_targets" {
-  for_each           = var.enable_ecs_schedule ? toset(local.ecs_services_to_schedule) : []  
-  max_capacity       = 2
-  min_capacity       = 0 
-  
+  for_each     = var.enable_ecs_schedule ? toset(local.ecs_services_to_schedule) : []
+  max_capacity = 2
+  min_capacity = 0
+
   resource_id        = "service/${aws_ecs_cluster.sd_cluster_dev.name}/${each.value}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
@@ -26,14 +26,14 @@ resource "aws_appautoscaling_target" "ecs_targets" {
 
 # Action to turn off at 21:00 UTC
 resource "aws_appautoscaling_scheduled_action" "scale_down_night" {
-  for_each           = aws_appautoscaling_target.ecs_targets
+  for_each = aws_appautoscaling_target.ecs_targets
 
   name               = "scale-down-${each.key}-23pm"
   service_namespace  = each.value.service_namespace
   resource_id        = each.value.resource_id
   scalable_dimension = each.value.scalable_dimension
-  
-  schedule           = "cron(0 21 * * ? *)" 
+
+  schedule = "cron(0 21 * * ? *)"
 
   scalable_target_action {
     min_capacity = 0
@@ -43,17 +43,17 @@ resource "aws_appautoscaling_scheduled_action" "scale_down_night" {
 
 # Action to turn on at 07:00 UTC
 resource "aws_appautoscaling_scheduled_action" "scale_up_morning" {
-  for_each           = aws_appautoscaling_target.ecs_targets
+  for_each = aws_appautoscaling_target.ecs_targets
 
   name               = "scale-up-${each.key}-9am"
   service_namespace  = each.value.service_namespace
   resource_id        = each.value.resource_id
   scalable_dimension = each.value.scalable_dimension
-  
-  schedule           = "cron(0 7 * * ? *)"
+
+  schedule = "cron(0 7 * * ? *)"
 
   scalable_target_action {
     min_capacity = 1
-    max_capacity = 2 
+    max_capacity = 2
   }
 }
